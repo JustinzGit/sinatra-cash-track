b0c97c553d99744f71af3ebd7b257050e9fe468b
Justin <justinms@outlook.com>
2020-04-06 13:10:22 -0500

    add route to authenticate and log user in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,17 @@
 class SessionsController < ApplicationController
 
-  get '/login' do 
+  get '/login' do
     erb :'/sessions/login'
+  end
+
+  post '/login' do
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect :'users/index'
+    else
+      flash[:warning] = "Something went wrong. Check credentials and try again."
+      redirect '/login'
+    end
   end
 end
