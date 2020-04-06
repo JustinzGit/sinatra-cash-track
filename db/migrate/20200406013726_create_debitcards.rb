3d92cb337f6436ca03be6baf025350c7fe636200
Justin <justinms@outlook.com>
2020-04-05 20:38:22 -0500

    add debitcards migration

---
+++ db/migrate/20200406013726_create_debitcards.rb
@@ -0,0 +1,10 @@
+class CreateDebitcards < ActiveRecord::Migration[6.0]
+  def change
+    create_table :banks do |t|
+      t.string :balance
+      t.integer :user_id
+      t.integer :bank_id
+      t.timestamps null: false
+    end
+  end
+end
