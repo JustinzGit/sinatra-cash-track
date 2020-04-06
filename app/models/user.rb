8923570351015ab41de1074930ee780c7b750736
Justin <justinms@outlook.com>
2020-04-06 09:41:49 -0500

    add has_secure_password macro

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   has_many :creditcards
   has_many :user_banks
   has_many :banks, through: :user_banks
+
+  # Macro from active record that provides password functionality
+  # Given access to authenticate method from activerecord that relies on Bcrypt
+  has_secure_password
 end
