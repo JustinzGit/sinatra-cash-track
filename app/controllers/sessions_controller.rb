c92d54cd698013997640c0f16cca50ddc383ab8d
Justin <justinms@outlook.com>
2020-04-06 13:31:09 -0500

    add route to create user account and log user in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,20 @@ class SessionsController < ApplicationController
     erb :'/sessions/signup'
   end
 
+  post '/signup' do
+    # Checks if email has alredy been taken
+    if User.find_by(email: params[:user][:email])
+      flash[:warning] = "That email has already been taken"
+
+    # Checks that all attributes have been filled, create user account and log user in
+    elsif params[:user][:username] != "" && params[:user][:email] != "" && params[:user][:password] != ""
+      user = User.create(params[:user])
+      session[:user_id] = user.id
+      flash[:notice] = "Welcome to Budget Tracker!"
+      redirect "/users/#{user.slug}"
+    end
+  end
+
   get '/login' do
     erb :'/sessions/login'
   end
