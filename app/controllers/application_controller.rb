1c439b3db9d6fba31e838e7d4824a4bb58f56986
Justin <justinms@outlook.com>
2020-04-06 14:07:55 -0500

    add helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,21 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
     erb :index
-  end 
+  end
+
+  helpers do
+    def current_user
+      User.find(session[:user_id])
+    end
+
+    def logged_in?
+      !!current_user
+    end
+
+    def redirect_if_not_logged_in
+      if !logged_in?
+        flash[:warning] = "You must be logged in!"
+      end 
+    end
+  end
 end
