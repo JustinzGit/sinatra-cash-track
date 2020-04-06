9b5d48a86d5d3dea8d5a06bffb694f1757993340
Justin <justinms@outlook.com>
2020-04-06 16:33:19 -0500

    add routes to create a new bill

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -2,4 +2,17 @@ class BillsController < ApplicationController
   get '/bills' do
     erb :'/bills/show'
   end
+
+  get '/bills/new' do
+    erb :'/bills/new'
+  end
+
+  post '/bills' do
+    Bill.create(params[:bill])
+    redirect '/bills'
+  end
+
+  get '/bills/:id/edit' do
+    erb :'/bills/edit'
+  end 
 end
