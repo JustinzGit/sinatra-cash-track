82712e34542f243a3efb3a03fcbcca0a9a6d3860
Justin <justinms@outlook.com>
2020-04-05 21:19:14 -0500

    add application controller, set configurations

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+class ApplicationController < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, "string to be hexed"
+    set :views, "app/views"
+    set :public_folder, "public"
+    register Sinatra::Flash
+  end 
+end
