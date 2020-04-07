1e61084350ba84ef7acd68e9a0fab11461c1fbe2
Justin <justinms@outlook.com>
2020-04-06 19:13:03 -0500

    add banks controller and show page route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,12 +12,12 @@ class ApplicationController < Sinatra::Base
       redirect "/users/#{current_user.slug}"
     else
       erb :index
-    end 
+    end
   end
 
   helpers do
     def current_user
-      User.find(session[:user_id])
+      User.find_by(id: session[:user_id])
     end
 
     def logged_in?
