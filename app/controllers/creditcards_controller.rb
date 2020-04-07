3d25ca24b46b93aa0939487fedb80cacbf7733fb
Justin <justinms@outlook.com>
2020-04-06 20:57:41 -0500

    add route to create new creditcard

--- app/controllers/creditcards_controller.rb
+++ app/controllers/creditcards_controller.rb
@@ -1,5 +1,9 @@
 class CreditcardsController < ApplicationController
   get '/creditcards' do
     erb :'/creditcards/show'
+  end
+
+  get '/creditcards/new' do
+    erb :'/creditcards/new'
   end 
 end
