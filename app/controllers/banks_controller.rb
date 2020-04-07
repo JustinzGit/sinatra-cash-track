a0a08e8c5c638044df202947189bd06185d38b1f
Justin <justinms@outlook.com>
2020-04-06 20:58:25 -0500

    added 6 routes for showing, creating, updatinga and deleting bank

---
+++ app/controllers/banks_controller.rb
@@ -0,0 +1,31 @@
+class BanksController < ApplicationController
+  get '/banks' do
+    erb :'/banks/show'
+  end
+
+  get '/banks/new' do
+    erb :'/banks/new'
+  end
+
+  post '/banks' do
+    current_user.banks << Bank.create(params[:bank])
+    redirect '/banks'
+  end
+
+  get '/banks/:id/edit' do
+    @bank = Bank.find(params[:id])
+    erb :'/banks/edit'
+  end
+
+  patch '/banks/:id' do
+    bank = Bank.find(params[:id])
+    bank.update(params[:bank])
+    redirect '/banks'
+  end
+
+  delete '/banks/:id' do
+    bank = Bank.find(params[:id])
+    bank.destroy
+    redirect '/banks'
+  end
+end
