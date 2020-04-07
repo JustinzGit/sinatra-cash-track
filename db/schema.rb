af3f0ac4f12debc423f526b0e860e4eac296b51c
Justin <justinms@outlook.com>
2020-04-06 21:01:46 -0500

    an update to db schema via migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_06_035646) do
+ActiveRecord::Schema.define(version: 2020_04_06_215726) do
 
   create_table "banks", force: :cascade do |t|
     t.string "name"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2020_04_06_035646) do
     t.string "provider"
     t.float "balance"
     t.string "duedate"
-    t.boolean "paid?"
+    t.boolean "paid?", default: false, null: false
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
