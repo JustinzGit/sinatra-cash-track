0b72405daee6c7983ec95eb4599a123de11b3a9c
Justin <justinms@outlook.com>
2020-04-09 11:16:32 -0500

    add redirect to users route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-
   get '/users/:slug' do
+    redirect_login_if_not_logged_in
     @user = User.find_user_by_slug(params[:slug])
     erb :'/users/show'
   end
