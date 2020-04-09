17be8ebc2d7db35975499fedff83c50b4ed43084
Justin <justinms@outlook.com>
2020-04-09 12:06:11 -0500

    add redirects if not logged in or authorized

--- app/controllers/creditcards_controller.rb
+++ app/controllers/creditcards_controller.rb
@@ -1,20 +1,23 @@
 class CreditcardsController < ApplicationController
   get '/creditcards' do
+    redirect_login_if_not_logged_in
     erb :'/creditcards/show'
   end
 
   get '/creditcards/new' do
+    redirect_login_if_not_logged_in
     erb :'/creditcards/new'
-  end
-
-  get '/creditcards/:id/edit' do
-    @creditcard = Creditcard.find(params[:id])
-    erb :'/creditcards/edit'
   end
 
   post '/creditcards' do
     current_user.creditcards << Creditcard.create(params[:creditcard])
     redirect '/creditcards'
+  end
+
+  get '/creditcards/:id/edit' do
+    @creditcard = Creditcard.find(params[:id])
+    redirect_if_not_authorized(current_user.creditcards, @creditcard)
+    erb :'/creditcards/edit'
   end
 
   patch '/creditcards/:id' do
